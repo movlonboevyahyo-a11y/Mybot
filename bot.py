682e18cefd16e4afb0648265d3406c6a89824c0f
movlonboevyahyo-a11y <movlonboevyahyo@gmail.com>
2025-11-14 17:26:00 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,24 @@
 import telebot
+from flask import Flask, request
 
 TOKEN = "7371202845:AAGZKymwF1eBsfYVisQ1M0DnElOz_LRZfr0"
-
 bot = telebot.TeleBot(TOKEN)
+
+app = Flask(__name__)
+
+# Telegram webhook uchun
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))]
+    )
+    return "!", 200
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://mybot-vj1z.onrender.com/' + TOKEN)
+    return "Webhook ishladi!", 200
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -11,7 +27,3 @@ def start(message):
 @bot.message_handler(func=lambda m: True)
 def echo(message):
     bot.send_message(message.chat.id, message.text)
-
-print("Bot ishga tushdi...")
-
-bot.infinity_polling()
