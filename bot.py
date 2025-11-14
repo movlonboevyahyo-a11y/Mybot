9ac4b40bffdb6447db7d1e28c1b5501adf7799ec
movlonboevyahyo-a11y <movlonboevyahyo@gmail.com>
2025-11-14 19:13:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,3 @@ def start(message):
 def echo(message):
     bot.send_message(message.chat.id, message.text)
 import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
